fc3593dfaf6fd5b2b7c49252d1268e795e72400b
Sam Gillenwater <copperheadlincoln@gmail.com>
2023-07-03 03:10:42 -0500

    Changed the url and tried to run again after reinstalling pymongo and restarting. Still no luck.

--- module_5/module_5_3/pytech_insert.py
+++ module_5/module_5_3/pytech_insert.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 
 # Connect to the MongoDB database
-client = MongoClient("mongodb://localhost:27017/")
+client = MongoClient("mongodb+srv://admin:admin@cluster0.2agz2qq.mongodb.net/")
 db = client.pytech
 students = db.students
 
