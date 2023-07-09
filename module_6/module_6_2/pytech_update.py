fc1e095ee0c2fd7e32c9f05bc23d9d7cf25e2de4
Sam Gillenwater <copperheadlincoln@gmail.com>
2023-07-09 18:13:23 -0500

    I added a block of info in the beginning for it to show that this is something more unique to me, something that I will be incorporating more into my work, I suppose.

---
+++ module_6/module_6_2/pytech_update.py
@@ -0,0 +1,41 @@
+"""
+        Gillenwater, Sam
+
+        CYBR410 - Prof Haas
+
+MongoDB Student Data Management
+
+This script connects to a MongoDB database and performs operations on a collection named 'students'.
+It retrieves all documents from the collection, updates the last name for a specific student, and displays the updated document.
+
+Prerequisites!:
+- pymongo library must be installed (install via 'pip install pymongo')
+
+Instructions:
+1. Ensure you have the necessary MongoDB connection string.
+2. Update the connection string in the 'client' variable to point to your MongoDB cluster.
+3. Run the script and observe the output.
+
+Note: This code assumes you have a MongoDB collection named 'students' with appropriate documents.
+"""
+
+from pymongo import MongoClient
+
+# Connect to the MongoDB database
+client = MongoClient("mongodb+srv://admin:admin@cluster0.2agz2qq.mongodb.net/")
+db = client.pytech
+students = db.students
+
+# Display all documents in the collection
+print("All documents in the collection:")
+docs = students.find({})
+for doc in docs:
+    print(doc)
+
+# Update the last name for student_id 1007
+students.update_one({"student_id": 1007}, {"$set": {"last_name": "O'Neal"}})
+
+# Display the updated document
+print("\nUpdated document for student_id 1007:")
+doc = students.find_one({"student_id": 1007})
+print(doc)
