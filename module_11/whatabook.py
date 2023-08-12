baecbaa13ca2e3f159a1b82644aabb04d7f70654
Sam Gillenwater <copperheadlincoln@gmail.com>
2023-08-12 18:56:38 -0500

    Changes made for error handling.

--- module_11/whatabook.py
+++ module_11/whatabook.py
@@ -12,38 +12,49 @@ DB_PASS = "MySQL8IsGreat!"
 DB_NAME = "whatabook"
 
 
-# Function to register a new user
+def get_integer_input(prompt):
+    """Utility function to get integer input from user with error handling."""
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            print("Please enter a valid integer.")
+
+
 def register_user(cursor):
-    # Get user details
+    """Function to register a new user."""
     email = input("Enter your email: ")
     first_name = input("Enter your first name: ")
     last_name = input("Enter your last name: ")
 
-    # Insert the new user into the User table
-    cursor.execute("INSERT INTO User(email, first_name, last_name) VALUES (%s, %s, %s);",
-                   (email, first_name, last_name))
-    print("Registration successful!")
+    try:
+        cursor.execute("INSERT INTO User(email, first_name, last_name) VALUES (%s, %s, %s);",
+                       (email, first_name, last_name))
+        print("Registration successful!")
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+        print("Registration failed. Please try again.")
 
 
-# Function to display all books
 def show_books(cursor):
+    """Function to display all books."""
     cursor.execute("SELECT * FROM Book;")
     books = cursor.fetchall()
     for book in books:
         print(book)
 
 
-# Function to display all store locations and their hours
 def show_locations(cursor):
+    """Function to display all store locations and their hours."""
     cursor.execute("SELECT store_name, store_hours FROM Store;")
     stores = cursor.fetchall()
     for store in stores:
         print(f"Store: {store[0]}, Hours: {store[1]}")
 
 
-# Function to validate the user's ID
 def validate_user(cursor):
-    user_id = int(input("Enter user_id: "))
+    """Function to validate the user's ID."""
+    user_id = get_integer_input("Enter user_id: ")
     cursor.execute("SELECT * FROM User WHERE user_id = %s;", (user_id,))
     if cursor.fetchone():
         return user_id
@@ -52,23 +63,23 @@ def validate_user(cursor):
         return None
 
 
-# Function to display the account menu
 def show_account_menu():
+    """Function to display the account menu."""
     print("1. Wishlist")
     print("2. Add Book")
     print("3. Main Menu")
 
 
-# Function to display the user's wishlist
 def show_wishlist(cursor, user_id):
+    """Function to display the user's wishlist."""
     cursor.execute("SELECT * FROM Wishlist WHERE user_id = %s;", (user_id,))
     wishlist = cursor.fetchall()
     for item in wishlist:
         print(item)
 
 
-# Function to display books available to add to the wishlist
 def show_books_to_add(cursor, user_id):
+    """Function to display books available to add to the wishlist."""
     cursor.execute("""
     SELECT * FROM Book
     WHERE book_id NOT IN (SELECT book_id FROM Wishlist WHERE user_id = %s);
@@ -78,8 +89,14 @@ def show_books_to_add(cursor, user_id):
         print(book)
 
 
-# Function to add a selected book to the user's wishlist
-def add_book_to_wishlist(cursor, user_id, book_id):
+def add_book_to_wishlist(cursor, user_id):
+    """Function to add a selected book to the user's wishlist."""
+    book_id = get_integer_input("Select a book to add to your wishlist: ")
+    cursor.execute("SELECT * FROM Book WHERE book_id = %s;", (book_id,))
+    if not cursor.fetchone():
+        print("Invalid book_id!")
+        return
+
     cursor.execute("SELECT * FROM Wishlist WHERE user_id = %s AND book_id = %s;", (user_id, book_id))
     if cursor.fetchone():
         print("This book is already in your wishlist!")
@@ -88,29 +105,30 @@ def add_book_to_wishlist(cursor, user_id, book_id):
         print("Book added to wishlist!")
 
 
-# Main function to run the application
 def main():
-    # Connect to the database
-    connection = mysql.connector.connect(
-        host=DB_HOST,
-        user=DB_USER,
-        password=DB_PASS,
-        database=DB_NAME
-    )
+    """Main function to run the application."""
+    try:
+        connection = mysql.connector.connect(
+            host=DB_HOST,
+            user=DB_USER,
+            password=DB_PASS,
+            database=DB_NAME
+        )
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+        sys.exit(1)
+
     cursor = connection.cursor()
 
-    # Main loop for user interaction
     while True:
-        # Display the main menu
         print("0. Register")
         print("1. View Books")
         print("2. View Store Locations")
         print("3. My Account")
         print("4. Exit Program")
 
-        choice = int(input("Enter your choice: "))
+        choice = get_integer_input("Enter your choice: ")
 
-        # Handle user choices
         if choice == 0:
             register_user(cursor)
         elif choice == 1:
@@ -121,24 +139,21 @@ def main():
             user_id = validate_user(cursor)
             if user_id:
                 show_account_menu()
-                account_choice = int(input("Enter your choice: "))
+                account_choice = get_integer_input("Enter your choice: ")
 
                 if account_choice == 1:
                     show_wishlist(cursor, user_id)
                 elif account_choice == 2:
                     show_books_to_add(cursor, user_id)
-                    book_id = int(input("Select a book to add to your wishlist: "))
-                    add_book_to_wishlist(cursor, user_id, book_id)
+                    add_book_to_wishlist(cursor, user_id)
                 elif account_choice == 3:
                     continue
         elif choice == 4:
             print("Exiting the program...")
             break
 
-    # Close the database connection
     connection.close()
 
 
-# Run the application
 if __name__ == "__main__":
     main()
